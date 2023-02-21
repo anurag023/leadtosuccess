2e125c15184a2b9923565997f60f64347f13c8c6
Anurag <theunstoppableanurag@gmail.com>
2023-02-21 23:56:32 +0530

    leadtosuccess complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 const path = require("path");
 const fs = require("fs");
 const app = express();
@@ -9,7 +10,7 @@ mongoose.set('strictQuery', true);
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/dancecontact');
+  await mongoose.connect("process.env.MONGO_URI");
   
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
